f6c019f4adea797ba96b7038e0b90fb726bea922
MarkYonan238 <92487308+MarkYonan238@users.noreply.github.com>
2021-10-14 12:12:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -254,7 +254,7 @@ if rad == 'Soorten Voertuigen':
     st.header('Visualisaties Voertuigen')
     st.markdown('#')
     st.subheader('Aantal Voertuigen per Brandstof Categorie')
-    'In deze grafiek is voor elke brandstof categorie een lijn te zien. Deze geeft de groei aan van de verkochte voertuigen (over de afgelopen vijf jaar) op een logaritmische schaalverdeling.'
+    'In deze grafiek is voor elke brandstof categorie een lijn te zien. Deze geeft de groei aan van de verkochte voertuigen (over de afgelopen vier jaar) op een logaritmische schaalverdeling.'
     ##############################################################################################################
     cm = pd.read_csv('cmm.csv')
     cm2 = pd.read_csv('cmm2.csv')
